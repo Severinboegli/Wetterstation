8fa867fcfb79783a6cfaa0b27355381f9e28ddc7
Severinboegli <severinboegli@outlook.de>
2023-06-16 15:54:42 +0200

    Anpassung Webseite

--- Website/PHP/currentDatas.php
+++ Website/PHP/currentDatas.php
@@ -18,11 +18,10 @@ $sql = "select * from datas order by datas_ID desc limit 1";
 $result = mysqli_query($conn, $sql);
 
 while ($row = mysqli_fetch_array($result)) {
-    echo "<p class='datas' >" . $row['temp'] . "° Grad</p>";
-    echo "<p class='datas' >" . $row['pressure'] . "° Bar</p>";
-    echo "<p class='datas' >" . $row['humidity'] . "</p>";
+    echo "<p class='datas' >" . $row['temp'] . "° C</p>";
+    echo "<p class='datas' >" . $row['pressure'] / 100 . " hPa</p>";
+    //echo "<p class='datas' >" . $row['humidity'] . "</p>";
 }
 
 mysqli_close($conn);
-
 ?>
