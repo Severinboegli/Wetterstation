86abbad0835d0922568637e512f472ed098db7a4
Severinboegli <severinboegli@outlook.de>
2023-06-23 09:42:31 +0200

    Update Data.sql

--- data.sql
+++ data.sql
@@ -1,3 +1,4 @@
+drop database if exists wetterstation;
 create database wetterstation;
 use wetterstation;
 
@@ -9,8 +10,3 @@ create table datas(
     humidity float,
     primary key(datas_ID)
 );
-
-insert into datas values(null,recorded_time,temp,pressure,humidity);
-
-// leeren einer Tabelle
-//DELETE FROM <Tablename>;
