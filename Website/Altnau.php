2947f71f07e2519ba29f6c655e0436482873e960
Severinboegli <severinboegli@outlook.de>
2023-07-06 09:51:37 +0200

    Update

--- Website/Altnau.php
+++ Website/Altnau.php
@@ -57,7 +57,7 @@
         </div>
 
 
-        <div class="table">
+        <div id="table">
         </div>
 
 
--- Website/Illighausen.php
+++ Website/Illighausen.php
@@ -56,7 +56,7 @@
         </div>
 
 
-        <div class="table">
+        <div id="table">
 
             </table>
 
